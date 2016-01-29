74e47fa058d22e1493a7fc90a8541111464383b4
Srikanth Pragada <srikanthpragada@gmail.com>
2016-01-29 09:35:13 +0530

    A custom attribute - AuthorAttribute

--- AuthorAttribute.cs
+++ AuthorAttribute.cs
@@ -2,5 +2,5 @@
     class AuthorAttribute : Attribute
     {
         public string Author  { get; set; }
-        public int    Version { get; set; }
+        public int Version { get; set; } = 1;  // default version 
     }
